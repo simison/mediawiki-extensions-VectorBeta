3d96a10e5dbf6e0260de9f1be30c029a53c95250
Trevor Parscal <trevorparscal@gmail.com>
2013-10-08 14:35:18 -0700

    Initial commit
    Adds access to turning on vector.skin-beta styles within the BetaFeatures preferences.
    
    Change-Id: I4fd58550b602651901e6af2fd4a055f5b09d4ffd

---
+++ VectorBeta.i18n.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ * This file is part of the MediaWiki extension VectorBeta.
+ *
+ * VectorBeta is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * VectorBeta is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VectorBeta.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @file
+ * @ingroup extensions
+ */
+
+$messages = array();
+
+/**
+ * English
+ */
+$messages['en'] = array(
+	'vector-beta-feature-typography-message' => 'Vector typography refresh',
+	'vector-beta-feature-typography-description' => 'Updates typography of the Vector skin to improve readability, accessibility and consistency.',
+);
+
+/**
+ * Message documentation (Message documentation)
+ */
+$messages['qqq'] = array(
+	'vector-beta-feature-typography-message' => 'Name shown in user preferences for Vector typography beta feature experiment.',
+	'vector-beta-feature-typography-description' => 'Brief explanation of Vector typography beta feature experiment.',
+);
