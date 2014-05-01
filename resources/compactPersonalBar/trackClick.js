81d677f350986febc94c098d98bc95665a21f6d6
Ori Livneh <ori@wikimedia.org>
2014-05-01 14:15:56 -0700

    Make localStorage use resilient
    localStorage.setItem is liable to fail even if localStorage feature detection
    indicates the browser supports localStorage. This can happen, for example, if
    localStorage is full. This mandates that any attempt to set a key in
    localStorage must be enclosed in a try / catch block. Since that is the case,
    it is silly to have a separate feature detection function that does the same
    thing. So combine the try / catch with the actual event storage.
    
    Bug: 64716
    Change-Id: I92de39ec0cca0c3dc5bb476f746adde2e59271e4

--- resources/compactPersonalBar/trackClick.js
+++ resources/compactPersonalBar/trackClick.js
@@ -4,38 +4,32 @@
 	mw.beta = mw.beta || {};
 	mw.beta.trackClick = $.noop;
 
-	// https://github.com/Modernizr/Modernizr/blob/master/feature-detects/storage/localstorage.js
-	function supportsLocalStorage() {
-		var mod = 'modernizr';
-		try {
-			localStorage.setItem(mod, mod);
-			localStorage.removeItem(mod);
-			return true;
-		} catch(e) {
-			return false;
-		}
-	}
-
 	function logSavedClick() {
-		var
-			schemaName = localStorage.getItem( 'trackClickSchemaName' ),
-			data = localStorage.getItem( 'trackClickData' );
+		var schemaName, data;
 
-		if ( schemaName ) {
-			mw.eventLog.logEvent( schemaName, JSON.parse( data ) );
-			localStorage.removeItem( 'trackClickSchemaName' );
-			localStorage.removeItem( 'trackClickData' );
-		}
+		try {
+			schemaName = localStorage.getItem( 'trackClickSchemaName' );
+			data = localStorage.getItem( 'trackClickData' );
+			if ( schemaName ) {
+				mw.eventLog.logEvent( schemaName, JSON.parse( data ) );
+				localStorage.removeItem( 'trackClickSchemaName' );
+				localStorage.removeItem( 'trackClickData' );
+			}
+		} catch ( e ) {}
 	}
 
-	if ( supportsLocalStorage() && mw.eventLog ) {
+	if ( mw.eventLog ) {
 
 		mw.beta.trackClick = function( el, schemaName, data ) {
 			$( el ).on( 'click', function() {
-				localStorage.setItem( 'trackClickSchemaName', schemaName );
-				localStorage.setItem( 'trackClickData', JSON.stringify( data ) );
 				// schedule sending the event if the click target was not a link,
 				// a link that was prevented or a link that only changed the anchor
+				try {
+					localStorage.setItem( 'trackClickSchemaName', schemaName );
+					localStorage.setItem( 'trackClickData', JSON.stringify( data ) );
+				} catch ( e ) {
+					return;
+				}
 				setTimeout( logSavedClick, 2000 );
 			} );
 		};
